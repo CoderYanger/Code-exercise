03ab6edae56a90c8501740481aa3b1356b1167f1
miraitowa6 <2968941520@qq.com>
2025-09-25 19:11:39 +0800

    58.最后一个单词的长度

---
+++ 0.刷题练习/P58/src/Solution.java
@@ -0,0 +1,45 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 王洋
+ * Date: 2025-08-12
+ * Time: 07:14
+ */
+class Solution {
+    // 58. 最后一个单词的长度
+    /*给你一个字符串 s，由若干单词组成，单词前后用一些空格字符隔开。返回字符串中 最后一个 单词的长度。
+    单词 是指仅由字母组成、不包含任何空格字符的最大子字符串。
+    示例 1：
+    输入：s = "Hello World"
+    输出：5
+    解释：最后一个单词是“World”，长度为 5。
+    示例 2：
+    输入：s = "   fly me   to   the moon  "
+    输出：4
+    解释：最后一个单词是“moon”，长度为 4。
+    示例 3：
+    输入：s = "luffy is still joyboy"
+    输出：6
+    解释：最后一个单词是长度为 6 的“joyboy”。
+    提示：
+            1 <= s.length <= 104
+    s 仅有英文字母和空格 ' ' 组成
+    s 中至少存在一个单词*/
+    public int lengthOfLastWord(String s) {
+        int length=0;
+        int i=s.length()-1;
+        while(i>0&&s.charAt(i)==' '){
+            i--;
+        }
+        //i>=0要放在前面判断
+        while(i>=0&&s.charAt(i)!=' '){
+            length++;
+            i--;
+        }
+        return length;
+    }
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.lengthOfLastWord("a"));
+    }
+}
