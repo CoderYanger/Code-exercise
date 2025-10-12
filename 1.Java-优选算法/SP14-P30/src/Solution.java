9ca7175a94ea00d5922cf02d149e131976a8b15e
miraitowa6 <2968941520@qq.com>
2025-10-12 10:17:15 +0800

    30.串联所有单词的子串

---
+++ 1.Java-优选算法/SP14-P30/src/Solution.java
@@ -0,0 +1,66 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 王洋
+ * Date: 2025-08-25
+ * Time: 22:01
+ */
+public class Solution {
+    //30. 串联所有单词的子串
+    /*给定一个字符串 s 和一个字符串数组 words。 words 中所有字符串 长度相同。
+    s 中的 串联子串 是指一个包含  words 中所有字符串以任意顺序排列连接起来的子串。
+    例如，如果 words = ["ab","cd","ef"]， 那么 "abcdef"， "abefcd"，"cdabef"， "cdefab"，"efabcd"， 和 "efcdab" 都是串联子串。 "acdbef" 不是串联子串，因为他不是任何 words 排列的连接。
+    返回所有串联子串在 s 中的开始索引。你可以以 任意顺序 返回答案。
+    示例 1：
+    输入：s = "barfoothefoobarman", words = ["foo","bar"]
+    输出：[0,9]
+    解释：因为 words.length == 2 同时 words[i].length == 3，连接的子字符串的长度必须为 6。
+    子串 "barfoo" 开始位置是 0。它是 words 中以 ["bar","foo"] 顺序排列的连接。
+    子串 "foobar" 开始位置是 9。它是 words 中以 ["foo","bar"] 顺序排列的连接。
+    输出顺序无关紧要。返回 [9,0] 也是可以的。
+    示例 2：
+    输入：s = "wordgoodgoodgoodbestword", words = ["word","good","best","word"]
+    输出：[]
+    解释：因为 words.length == 4 并且 words[i].length == 4，所以串联子串的长度必须为 16。
+    s 中没有子串长度为 16 并且等于 words 的任何顺序排列的连接。
+    所以我们返回一个空数组。
+    示例 3：
+    输入：s = "barfoofoobarthefoobarman", words = ["bar","foo","the"]
+    输出：[6,9,12]
+    解释：因为 words.length == 3 并且 words[i].length == 3，所以串联子串的长度必须为 9。
+    子串 "foobarthe" 开始位置是 6。它是 words 中以 ["foo","bar","the"] 顺序排列的连接。
+    子串 "barthefoo" 开始位置是 9。它是 words 中以 ["bar","the","foo"] 顺序排列的连接。
+    子串 "thefoobar" 开始位置是 12。它是 words 中以 ["the","foo","bar"] 顺序排列的连接。
+    提示：
+            1 <= s.length <= 104
+            1 <= words.length <= 5000
+            1 <= words[i].length <= 30
+    words[i] 和 s 由小写英文字母组成*/
+    public List<Integer> findSubstring(String s, String[] words) {
+        List <Integer> list=new ArrayList<>();
+        HashMap<String,Integer> hash1=new HashMap<>();
+        char[] ss=s.toCharArray();
+        int n=ss.length;int m=words.length;int len=words[0].length();
+        for(String str:words) hash1.put(str,hash1.getOrDefault(str,0)+1);
+        for(int i=0;i<len;i++){
+            HashMap<String,Integer> hash2=new HashMap<>();
+            for(int left=i,right=i,count=0;right+len<=n;right+=len){//长度比下标多一个，所以加上等于
+                String in=s.substring(right,right+len);
+                hash2.put(in,hash2.getOrDefault(in,0)+1);
+                if(hash2.get(in)<=hash1.getOrDefault(in,0)) count++;
+                if(right-left+1>len*m){
+                    String out=s.substring(left,left+len);
+                    if(hash2.get(out)<=hash1.getOrDefault(out,0)) count--;
+                    hash2.put(out,hash2.get(out)-1);
+                    left+=len;
+                }
+                if(count==m) list.add(left);
+            }
+        }
+        return list;
+    }
+}
