97c5d4a9db10fe87a6466b02a8d2dd03507a588b
miraitowa6 <2968941520@qq.com>
2025-11-03 19:07:12 +0800

    1.两数之和

---
+++ 1.Java-优选算法/SP59-P1/src/Solution.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 王洋
+ * Date: 2025-09-14
+ * Time: 10:49
+ */
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        //按照吴小哲的思路自己写的
+        Map<Integer,Integer> hash =new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int search=target-nums[i];
+            if(!hash.containsKey(search)) hash.put(nums[i],i);
+            else return new int[]{i,hash.get(search)};
+        }
+        return new int[0];
+    }
+}
