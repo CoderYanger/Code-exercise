16ad774ba3a2965507c45c3eb447457d177def1f
miraitowa6 <2968941520@qq.com>
2025-11-19 21:07:04 +0800

    371.两整数之和

---
+++ 2.Java-递归、搜索与回溯算法/SP97-OJ4/src/Solution.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 王洋
+ * Date: 2025-09-22
+ * Time: 23:41
+ */
+class Solution {
+    public void hanota(List<Integer> A, List<Integer> B, List<Integer> C) {
+        //不讲武德
+        for(int x: A) C.add(x);
+    }
+}
