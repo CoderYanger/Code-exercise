d0043ec5260f34df113ed35bdee5092e4d66de66
miraitowa6 <2968941520@qq.com>
2025-12-06 08:29:01 +0800

    面试题17.16.按摩师

---
+++ 3.Java-动态规划算法/SP148-OJ17.16/src/Solution.java
@@ -0,0 +1,23 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 王洋
+ * Date: 2025-10-16
+ * Time: 22:46
+ */
+class Solution {
+    //面试题17.16. 按摩师
+    public int massage(int[] nums) {
+        int n=nums.length;
+        if(n==0) return 0;
+        int[] f=new int[n];
+        int[] g=new int[n];
+        g[0]=0;
+        f[0]=nums[0];
+        for(int i=1;i<n;i++){
+            f[i]=g[i-1]+nums[i];
+            g[i]=Math.max(f[i-1],g[i-1])+0;
+        }
+        return Math.max(g[n-1],f[n-1]);
+    }
+}
