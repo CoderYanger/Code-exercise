1ce9fb8c04b86c2cd8d9c6aa848aed18156d629c
miraitowa6 <2968941520@qq.com>
2026-03-05 12:32:59 +0800

    2154. 将找到的值乘以 2

--- 5.系统刷题/A.每日一题/A8-P2154/src/Solution.java
+++ 5.系统刷题/A.每日一题/A8-P2154/src/Solution.java
@@ -11,7 +11,7 @@ import java.util.Set;
  */
 class Solution {
     //2154. 将找到的值乘以 2
-    //排序解法
+    //解法一：排序+枚举
     public int findFinalValue(int[] nums, int original) {
         Arrays.sort(nums);
         for(int x:nums)
@@ -19,11 +19,32 @@ class Solution {
                 original*=2;
         return original;
     }
-
+    //解法二：哈希表
     public int findFinalValue1(int[] nums, int original) {
         Set<Integer> hash=new HashSet<>();
         for(int x:nums) hash.add(x);
         while(hash.contains(original)) original*=2;
         return original;
+    }
+
+
+    //解法三：位运算
+    public int findFinalValue(int[] nums, int original) {
+        //mask第k位为1，表示original×2ᵏ在数组中存在
+        int mask=0;
+        for(int x:nums){
+            //我们只关心x=original×2ᵏ这种形式的数
+            int k=x/original;
+            //满足以下两个条件
+            //①x是original的整数倍→能被整除
+            //②k是2的幂
+            //k&(k-1)：判断是不是2的幂的经典方法
+            if(x%original==0&&(k&(k-1))==0) 
+                //把mask里第k位设为1，表示original×2ᵏ存在
+                mask|=k;
+        }
+        //我们要找第一个不满足的：需要按位取反，取出最右侧的1，即为第一个不满足的
+        mask=~mask;
+        return original*(mask&-mask);
     }
 }
