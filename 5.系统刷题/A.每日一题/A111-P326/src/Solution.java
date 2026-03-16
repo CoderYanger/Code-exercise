07f2143af81ff6959056ba8d79dab77e3e467c34
miraitowa6 <2968941520@qq.com>
2026-03-16 08:05:06 +0800

    326.3的幂

---
+++ 5.系统刷题/A.每日一题/A111-P326/src/Solution.java
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 王洋
+ * Date: 2026-03-04
+ * Time: 14:03
+ */
+class Solution {
+    //326. 3 的幂
+    public boolean isPowerOfThree(int n) {
+        return n>0&&1162261467%n==0;
+    }
+}
