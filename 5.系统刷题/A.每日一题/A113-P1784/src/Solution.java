1b3027a442d499aa6ff8d7fa8eea95acb1939c4c
miraitowa6 <2968941520@qq.com>
2026-03-17 08:05:09 +0800

    1784.检查二进制字符串字段

---
+++ 5.系统刷题/A.每日一题/A113-P1784/src/Solution.java
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 王洋
+ * Date: 2026-03-06
+ * Time: 11:05
+ */
+class Solution {
+    //1784. 检查二进制字符串字段
+    public boolean checkOnesSegment(String s) {
+        return !s.contains("01");
+    }
+}
