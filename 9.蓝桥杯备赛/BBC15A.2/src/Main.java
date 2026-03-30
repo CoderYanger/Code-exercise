ad1a29a14106827d34187dd1fd509a6529894f0c
miraitowa6 <2968941520@qq.com>
2026-03-30 22:00:08 +0800

    第十五届蓝桥杯JavaA组2.召唤数学精灵

---
+++ 9.蓝桥杯备赛/BBC15A.2/src/Main.java
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 王洋
+ * Date: 2026-03-18
+ * Time: 13:37
+ */
+import java.util.Scanner;
+public class Main {
+    //召唤数学精灵
+    public static void main(String[] args) {
+        System.out.println(2024041331404202L/200L*4+2);
+    }
+}
