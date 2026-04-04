b31bbd58fcd132fb4ff5f7e5c8d3b877ce4a96e3
miraitowa6 <2968941520@qq.com>
2026-04-04 15:57:29 +0800

    2235.两整数相加

---
+++ 5.系统刷题/「新」动计划 · 编程入门/P2235/src/Solution.java
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 王洋
+ * Date: 2026-03-12
+ * Time: 20:12
+ */
+class Solution {
+    //2235. 两整数相加
+    //0ms击败100.00%
+    public int sum(int num1, int num2) {
+        return num1+num2;
+    }
+}
