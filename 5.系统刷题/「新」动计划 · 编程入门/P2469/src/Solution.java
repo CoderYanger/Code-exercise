8654a6722f96ceb3b0db9361a13ca6557e064c5b
miraitowa6 <2968941520@qq.com>
2026-04-04 22:00:06 +0800

    2469.温度转换

---
+++ 5.系统刷题/「新」动计划 · 编程入门/P2469/src/Solution.java
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 王洋
+ * Date: 2026-03-12
+ * Time: 20:13
+ */
+class Solution {
+    //2469. 温度转换
+    //0ms击败100.00%
+    public double[] convertTemperature(double c) {
+        return new double[]{c+273.15,c*1.80+32.00};
+    }
+}
