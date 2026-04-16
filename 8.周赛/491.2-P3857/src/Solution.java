a4dc7a9061c2ce37e2982eb19ba4cb3c2a2d48c8
miraitowa6 <2968941520@qq.com>
2026-04-16 22:00:12 +0800

    3857.拆分到1的最小总代价

---
+++ 8.周赛/491.2-P3857/src/Solution.java
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 王洋
+ * Date: 2026-03-01
+ * Time: 16:27
+ */
+class Solution {
+    //3857. 拆分到 1 的最小总代价
+    public int minCost(int n) {
+        return n*(n-1)/2;
+    }
+}
