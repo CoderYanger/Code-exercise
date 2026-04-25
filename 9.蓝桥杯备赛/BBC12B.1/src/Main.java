a0c6033d5c0c479b5e915266a8ace9cca0ba976f
miraitowa6 <2968941520@qq.com>
2026-04-25 08:05:08 +0800

    第十二届蓝桥杯JavaB组1.ASC

---
+++ 9.蓝桥杯备赛/BBC12B.1/src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 王洋
+ * Date: 2026-04-17
+ * Time: 18:36
+ */
+public class Main {
+    //ASC
+    public static void main(String[] args) {
+        System.out.println('L'-'A'+65);
+    }
+}
