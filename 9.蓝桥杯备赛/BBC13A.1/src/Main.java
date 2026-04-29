d6eeb5dc97db2e383982024cf6432c2665457ec3
miraitowa6 <2968941520@qq.com>
2026-04-29 08:05:08 +0800

    第十三届蓝桥杯JavaA组1.裁纸刀

---
+++ 9.蓝桥杯备赛/BBC13A.1/src/Main.java
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 王洋
+ * Date: 2026-04-17
+ * Time: 18:28
+ */
+import java.util.Scanner;
+public class Main {
+    //裁纸刀
+    public static void main(String[] args) {
+        System.out.println(4+19+20*21);
+    }
+}
