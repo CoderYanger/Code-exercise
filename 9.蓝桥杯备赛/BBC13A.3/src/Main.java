8154fd647ff3c39838fea2248defc6a7928f3f97
miraitowa6 <2968941520@qq.com>
2026-05-02 22:00:06 +0800

    第十三届蓝桥杯JavaA组3.求和

---
+++ 9.蓝桥杯备赛/BBC13A.3/src/Main.java
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 王洋
+ * Date: 2026-04-17
+ * Time: 18:30
+ */
+import java.util.Scanner;
+public class Main {
+    //求和
+    public static void main(String[] args) {
+        System.out.println(20230409L*20230408L/2);
+    }
+}
