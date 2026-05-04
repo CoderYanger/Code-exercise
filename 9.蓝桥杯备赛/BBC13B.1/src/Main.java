0ad14a68b15be934574bfdd63c7475a0e0362101
miraitowa6 <2968941520@qq.com>
2026-05-04 08:05:08 +0800

    第十三届蓝桥杯JavaB组1.星期计算

---
+++ 9.蓝桥杯备赛/BBC13B.1/src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 王洋
+ * Date: 2026-04-17
+ * Time: 18:32
+ */
+public class Main {
+    //星期计算
+    public static void main(String[] args) {
+        System.out.println((int)Math.pow(20,22)%7+6);
+    }
+}
