715a653cbe76811b3c53c268c1521edaa7ebe462
miraitowa6 <2968941520@qq.com>
2026-05-17 22:00:09 +0800

    2840.判断通过操作能否让字符串相等Ⅱ

---
+++ 5.系统刷题/A.每日一题/A132-P2840/src/Solution.java
@@ -0,0 +1,32 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 王洋
+ * Date: 2026-04-17
+ * Time: 18:48
+ */
+class Solution {
+    //2840. 判断通过操作能否让字符串相等II
+    public boolean checkStrings(String s1, String s2) {
+        int n=s1.length();
+        //奇数位哈希表
+        int[] odd=new int[26];
+        //偶数位哈希表
+        int[] even=new int[26];
+        for(int i=0;i<n;i++){
+            if(i%2==0){
+                even[s1.charAt(i)-'a']++;
+                even[s2.charAt(i)-'a']--;
+            }
+            else{
+                odd[s1.charAt(i)-'a']++;
+                odd[s2.charAt(i)-'a']--;
+            }
+        }
+        for(int i=0;i<26;i++){
+            if(odd[i]!=0) return false;
+            if(even[i]!=0) return false;
+        }
+        return true;
+    }
+}
