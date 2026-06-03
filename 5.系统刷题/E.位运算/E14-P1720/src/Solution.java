c0ef6946291b8a792bde9b68ff07f3c21cfd95b9
miraitowa6 <2968941520@qq.com>
2026-06-03 12:45:52 +0800

    1720.解码异或后的数组

---
+++ 5.系统刷题/E.位运算/E14-P1720/src/Solution.java
@@ -0,0 +1,18 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: CoderYanger
+ * Date: 2026-05-14
+ * Time: 12:59
+ */
+class Solution {
+    //1720. 解码异或后的数组
+    public int[] decode(int[] encoded, int first) {
+        //异或消消乐
+        int n=encoded.length;
+        int[] ret=new int[n+1];
+        ret[0]=first;
+        for(int i=1;i<n+1;i++) ret[i]=encoded[i-1]^ret[i-1];
+        return ret;
+    }
+}
