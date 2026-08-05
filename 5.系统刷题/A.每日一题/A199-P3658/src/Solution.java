5b8e62d2320cce0c10891c643e1c3f179cb0c651
miraitowa6 <2968941520@qq.com>
2026-08-05 10:55:24 +0800

    3658.奇数和与偶数和的最大公约数

---
+++ 5.系统刷题/A.每日一题/A199-P3658/src/Solution.java
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: CoderYanger
+ * Date: 2026-07-26
+ * Time: 15:25
+ */
+class Solution {
+    //3658. 奇数和与偶数和的最大公约数
+    public int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+}
