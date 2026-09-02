1ad8dffc2c04bbcbc632a43b2752726ffcf404a7
miraitowa6 <2968941520@qq.com>
2026-09-02 08:43:48 +0800

    3870.统计范围内的逗号

---
+++ 8.周赛/493.1-P3870/src/Solution.java
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 王洋
+ * Date: 2026-03-16
+ * Time: 18:07
+ */
+class Solution {
+    //3870. 统计范围内的逗号
+    public int countCommas(int n) {
+        return n<1000?0:n-999;
+    }
+}
