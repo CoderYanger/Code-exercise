9a29bf391b413c8cffbf5479eb0747ce5fa05d0d
miraitowa6 <2968941520@qq.com>
2026-09-04 18:49:12 +0800

    101018.构造奇偶一致的数组Ⅰ

---
+++ 8.周赛/494.1-P101018/src/Solution.java
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 王洋
+ * Date: 2026-03-22
+ * Time: 16:33
+ */
+class Solution {
+    //101018. 构造奇偶一致的数组 I
+    public boolean uniformArray(int[] nums1) {
+        return true;
+    }
+}
