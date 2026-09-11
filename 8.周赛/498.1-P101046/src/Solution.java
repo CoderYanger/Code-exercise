8fa0ea6245165aa90194ddc5c680151dca5003d9
miraitowa6 <2968941520@qq.com>
2026-09-11 11:32:25 +0800

    101046.最小稳定下标Ⅰ

---
+++ 8.周赛/498.1-P101046/src/Solution.java
@@ -0,0 +1,24 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 王洋
+ * Date: 2026-04-19
+ * Time: 13:37
+ */
+class Solution {
+    //101046. 最小稳定下标 I
+    public int firstStableIndex(int[] nums, int k) {
+        int n=nums.length;
+        if(n==1) return 0<=k?0:-1;
+        int[] max=new int[n];
+        max[0]=nums[0];
+        int[] min=new int[n];
+        min[n-1]=nums[n-1];
+        for(int i=n-2;i>=0;i--) min[i]=Math.min(nums[i],min[i+1]);
+        for(int i=0;i<n;i++){
+            if(i>0) max[i]=Math.max(nums[i],max[i-1]);
+            if(max[i]-min[i]<=k) return i;
+        }
+        return -1;
+    }
+}
